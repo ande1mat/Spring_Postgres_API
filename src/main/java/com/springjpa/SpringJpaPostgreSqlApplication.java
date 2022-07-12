50e1ef078978b212a86755de1b1fac404c575ed6
Matt Anderson <32207420+ande1mat@users.noreply.github.com>
2022-07-12 13:34:52 -0500

    Update SpringJpaPostgreSqlApplication.java

--- src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
+++ src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
@@ -3,13 +3,9 @@ package com.springjpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /*
-
 http://localhost:8090
-
  */
 
 @EnableCircuitBreaker
